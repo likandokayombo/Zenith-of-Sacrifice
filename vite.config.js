e07d0b67bb0acecc096ec4a5c61bea5aa02a1c8b
Likando Kayomboe <likandokayombo2030@gmail.com>
2025-06-26 15:11:05 +0200

    third change

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PAATH || "/zenith-of-sacrifice",
+  base: process.env.VITE_BASE_PATH || "/zenith-of-sacrifice",
 })
